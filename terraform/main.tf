41fe25943c834f8f10b7640eec843919817db4c5
Rajesh Tota <108001568+rajesh-tota@users.noreply.github.com>
2024-11-14 12:05:58 +0100

    Update main.tf
    Monitoring

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,3 +44,39 @@ resource "azurerm_app_service" "website" {
     scm_type         = "LocalGit"
   }
 }
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = "tota0610-lg-analytics"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+resource "azurerm_application_insights" "appi" {
+  name                = "totanov-api"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  workspace_id        = azurerm_log_analytics_workspace.log.id
+  application_type    = "web"
+}
+// Skeleton for linking web app and app insights
+resource "null_resource" "link_monitoring" {
+  provisioner "local-exec" {
+    command = <<EOT
+      # Login to Azure CLI (Linux operating system assumed)
+      az login --service-principal -u $con_client_id -p $con_client_secret --tenant $con_tenant_id
+      az webapp config appsettings set --name $web_app_name --resource-group $rg_name --settings APPINSIGHTS_INSTRUMENTATIONKEY=$inst_key APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 APPLICATIONINSIGHTS_CONNECTION_STRING=$conn_str ApplicationInsightsAgent_EXTENSION_VERSION=~3 DiagnosticServices_EXTENSION_VERSION=~3 InstrumentationEngine_EXTENSION_VERSION=disabled SnapshotDebugger_EXTENSION_VERSION=disabled XDT_MicrosoftApplicationInsights_BaseExtensions=recommended XDT_MicrosoftApplicationInsights_PreemptSdk=disabled
+      # TODO your scripting code
+    EOT
+    environment = {
+      // Parameters needed to login
+      con_client_id     = var.client_id
+      con_client_secret = var.client_secret
+      con_tenant_id     = var.tenant_id
+      // Parameters needed for linking
+      inst_key          = azurerm_application_insights.appi.instrumentation_key
+      conn_str          = azurerm_application_insights.appi.connection_string      
+      rg_name           = var.rg_name
+      web_app_name      = var.web_app_name
+    }
+  }
+}
