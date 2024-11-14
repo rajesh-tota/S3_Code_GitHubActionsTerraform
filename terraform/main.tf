0cf6796fc48d18fd7beb9da96ffc60c9aeb09ad9
Rajesh Tota <108001568+rajesh-tota@users.noreply.github.com>
2024-11-14 13:15:55 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,26 +65,4 @@ resource "azurerm_application_insights" "appi" {
   workspace_id        = azurerm_log_analytics_workspace.log.id
   application_type    = "web"
 }
-The first challenge is to provide a special null resource where our scripting code lives in. Below you find a first skeleton:
 
-
-resource "null_resource" "link_monitoring" {
-  provisioner "local-exec" {
-    command = <<EOT
-      # Login to Azure CLI (Linux operating system assumed)
-      az login --service-principal -u $con_client_id -p $con_client_secret --tenant $con_tenant_id
-      # TODO your scripting code
-    EOT
-    environment = {
-      // Parameters needed to login
-      con_client_id     = TODO
-      con_client_secret = TODO
-      con_tenant_id     = TODO
-      // Parameters needed for linking
-      inst_key          = TODO
-      conn_str          = TODO      
-      rg_name           = TODO
-      web_app_name      = TODO
-    }
-  }
-}
