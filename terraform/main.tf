01abb1b355dd5c80d6a4eea162f4eabd24c01728
Rajesh Tota <108001568+rajesh-tota@users.noreply.github.com>
2024-11-14 12:32:44 +0100

    Update main.tf
    new changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,13 @@ terraform {
 provider "azurerm" {
   features {}
 }
-#Get resource group
+
+# Get resource group
 data "azurerm_resource_group" "wsdevops" {
   name = var.rg_name
 }
+
+# App Service Plan
 resource "azurerm_app_service_plan" "sp1" {
   name                = var.app_service_plan_name
   location            = var.location
@@ -33,6 +36,8 @@ resource "azurerm_app_service_plan" "sp1" {
     size = "S1"
   }
 }
+
+# Web App
 resource "azurerm_app_service" "website" {
   name                = var.web_app_name
   location            = var.location
@@ -41,9 +46,18 @@ resource "azurerm_app_service" "website" {
 
   site_config {
     linux_fx_version = "NODE|22-lts"
-    scm_type         = "LocalGit"
+  }
+
+  app_settings = {
+    APPINSIGHTS_INSTRUMENTATIONKEY          = azurerm_application_insights.appi.instrumentation_key
+    APPINSIGHTS_PROFILERFEATURE_VERSION     = "1.0.0"
+    APPINSIGHTS_SNAPSHOTFEATURE_VERSION     = "1.0.0"
+    APPLICATIONINSIGHTS_CONNECTION_STRING   = azurerm_application_insights.appi.connection_string
+    ApplicationInsightsAgent_EXTENSION_VERSION = "~3"
   }
 }
+
+# Log Analytics Workspace
 resource "azurerm_log_analytics_workspace" "log" {
   name                = "tota0610-lg-analytics"
   location            = data.azurerm_resource_group.wsdevops.location
@@ -51,32 +65,12 @@ resource "azurerm_log_analytics_workspace" "log" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+
+# Application Insights
 resource "azurerm_application_insights" "appi" {
   name                = "totanov-api"
   location            = data.azurerm_resource_group.wsdevops.location
   resource_group_name = data.azurerm_resource_group.wsdevops.name
   workspace_id        = azurerm_log_analytics_workspace.log.id
   application_type    = "web"
-}
-// Skeleton for linking web app and app insights
-resource "null_resource" "link_monitoring" {
-  provisioner "local-exec" {
-    command = <<EOT
-      # Login to Azure CLI (Linux operating system assumed)
-      az login --service-principal -u $con_client_id -p $con_client_secret --tenant $con_tenant_id
-      az webapp config appsettings set --name $web_app_name --resource-group $rg_name --settings APPINSIGHTS_INSTRUMENTATIONKEY=$inst_key APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 APPLICATIONINSIGHTS_CONNECTION_STRING=$conn_str ApplicationInsightsAgent_EXTENSION_VERSION=~3 DiagnosticServices_EXTENSION_VERSION=~3 InstrumentationEngine_EXTENSION_VERSION=disabled SnapshotDebugger_EXTENSION_VERSION=disabled XDT_MicrosoftApplicationInsights_BaseExtensions=recommended XDT_MicrosoftApplicationInsights_PreemptSdk=disabled
-      # TODO your scripting code
-    EOT
-    environment = {
-      // Parameters needed to login
-      con_client_id     = var.client_id
-      con_client_secret = var.client_secret
-      con_tenant_id     = var.tenant_id
-      // Parameters needed for linking
-      inst_key          = azurerm_application_insights.appi.instrumentation_key
-      conn_str          = azurerm_application_insights.appi.connection_string      
-      rg_name           = var.rg_name
-      web_app_name      = var.web_app_name
-    }
-  }
 }
